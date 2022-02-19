c325edd470d54bd434d17102306cef60fe21ff98
Alasdair Smith <ali@alasdairsmith.co.uk>
2022-02-19 21:44:10 +0000

    Initial set up for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,13 @@
+import "./App.css";
+
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="page">
+      <h1 className="title">CYF Blocks</h1>
+
+      <div className="blockly-wrapper"></div>
+
+      <div className="output"></div>
+    </div>
+  );
 }
