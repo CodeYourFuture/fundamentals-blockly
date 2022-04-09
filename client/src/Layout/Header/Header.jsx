2239b6848a412e2e147897534ce8a4813c9c9919
Alasdair Smith <ali@alasdairsmith.co.uk>
2022-04-09 14:17:12 +0100

    Prettify

--- client/src/Layout/Header/Header.jsx
+++ client/src/Layout/Header/Header.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Button from "../../Button/Button";
 import "./Header.scss";
 
 const Header = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const toggleMenu = () => setOpen(!open);
+  const toggleMenu = () => setOpen(!open);
 
-
-   return( <header className="c-layout__header c-header">
-       <h1 className="c-header__title"> </h1>
-       <Button context="c-header__menu"  text='Menu' action={toggleMenu} />
-    </header>)
+  return (
+    <header className="c-layout__header c-header">
+      <h1 className="c-header__title"> </h1>
+      <Button context="c-header__menu" text="Menu" action={toggleMenu} />
+    </header>
+  );
 };
 export default Header;
--- client/src/Layout/Menu/Menu.jsx
+++ client/src/Layout/Menu/Menu.jsx
@@ -1,45 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Menu.scss";
-
 
 // I've put dummy text in assuming you have planned to extract these links and titles from the ExerciseIndex?
 const Menu = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const toggleMenu = () => setOpen(!open);
-    return (
-        <nav className="c-layout__menu c-menu" aria-label="Main Menu.">
-            <div className="c-menu__wrapper">
-                <h3 className="c-menu__heading">Building Blocks</h3>
-                <ol className="c-menu__list">
-                    {/* Link must both load this exercise and manage focus to the text */}
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                </ol>
-                <h3 className="c-menu__heading">Arrays</h3>
-                <ol className="c-menu__list">
-                    {/* Link must both load this exercise and manage focus to the text */}
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                </ol>
-                <h3 className="c-menu__heading">Projects</h3>
-                <ol className="c-menu__list">
-                    {/* Link must both load this exercise and manage focus to the text */}
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                    <li className="c-menu__item"><a className="c-menu__link" href="/">Dummy</a></li>
-                </ol>
-            </div>
-        </nav>
-    )
+  const toggleMenu = () => setOpen(!open);
+  return (
+    <nav className="c-layout__menu c-menu" aria-label="Main Menu.">
+      <div className="c-menu__wrapper">
+        <h3 className="c-menu__heading">Building Blocks</h3>
+        <ol className="c-menu__list">
+          {/* Link must both load this exercise and manage focus to the text */}
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+        </ol>
+        <h3 className="c-menu__heading">Arrays</h3>
+        <ol className="c-menu__list">
+          {/* Link must both load this exercise and manage focus to the text */}
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+        </ol>
+        <h3 className="c-menu__heading">Projects</h3>
+        <ol className="c-menu__list">
+          {/* Link must both load this exercise and manage focus to the text */}
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+          <li className="c-menu__item">
+            <a className="c-menu__link" href="/">
+              Dummy
+            </a>
+          </li>
+        </ol>
+      </div>
+    </nav>
+  );
 };
 
 export default Menu;
